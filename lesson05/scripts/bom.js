f84c0a4d450be39d592eec40545fc6b1c90280e7
mrbourdet <mrbourdet@yahoo.com>
2024-02-07 13:42:56 -0500

    js change

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -8,7 +8,7 @@ button.addEventListener('click', () => {
         const li = document.createElement('li');
         const deleteButton = document.createElement('button');
         li.textContent = input.value;
-        deleteButton = textContent = "❌";
+        deleteButton.textContent = "❌";
         li.append(deleteButton);
         list.append(li);
 
