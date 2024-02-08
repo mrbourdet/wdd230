8a8b5f87d9f9dd7501a93712bf1a60c3cf9c1626
mrbourdet <mrbourdet@yahoo.com>
2024-02-07 21:48:20 -0500

    push

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -3,14 +3,17 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 button.addEventListener('click', () => {
-	if (input.value !="") 
+	if (input.value != "") 
     {
         const li = document.createElement('li');
         const deleteButton = document.createElement('button');
         li.textContent = input.value;
         deleteButton.textContent = "❌";
+        deleteButton.setAttribute("aria-label", "Delete Button");
         li.append(deleteButton);
         list.append(li);
+        input.focus();
+        input.value = "";
 
         deleteButton.addEventListener('click', () => {
             list.removeChild(li);
