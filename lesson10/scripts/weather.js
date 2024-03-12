db783a4fe3271bfcc26db16027dd1547b7a7517f
mrbourdet <mrbourdet@yahoo.com>
2024-03-12 11:44:25 -0400

    weather

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,36 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const apiKey = "0dc2835c1f834a1874a6f13e7e20ccc9";
+const lat = "49.7557";
+const lon = "6.6394";
+const units = "imperial";
+const urlBase = `https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid={API key}&units=imperial`;
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+console.log(url);
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            //console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.txt());
+        }
+    }   catch (error) {
+        console.log(error);
+    }
+}
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
