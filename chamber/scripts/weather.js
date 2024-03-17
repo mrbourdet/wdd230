22910313fe75efd32313922c61df10187681df86
mrbourdet <mrbourdet@yahoo.com>
2024-03-17 02:50:04 -0400

    put

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -90,15 +90,8 @@ function displayForecast(foreData) {
         const p = document.createElement("p");
         p.textContent = `${dateString} ${timeString}:00 EDT; Temp: ${temperature}°F`;
         forecast.appendChild(p);
-        //console.log(fomattedDate);
      });
-//     currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
-//     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-//     let desc = data.weather[0].description;
-//     weatherIcon.setAttribute('src', iconsrc);
-//     weatherIcon.setAttribute('alt', desc);
-//     captionDesc.textContent = `${desc}`;
-//    captionDesc.textContent = `${desc}`;
+
 }
 
 function titleCase(str) {
